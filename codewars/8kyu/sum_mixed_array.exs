ceb5a966057def561a5e15b08ae50a709049e283
keliumJU <keliumju@gmail.com>
2022-11-24 23:21:45 -0500

    codewarsw

---
+++ codewars/8kyu/sum_mixed_array.exs
@@ -0,0 +1,16 @@
+defmodule SumMix do
+  def sumMin(list) do
+    Enum.map(list, fn x -> 
+      if is_bitstring(x) do
+        Integer.parse(x) |> elem(0) 
+      else
+        x
+      end
+    end) |> 
+    Enum.sum()
+  end
+end
+
+IO.puts SumMix.sumMin([1,2,3])
+IO.puts SumMix.sumMin(["1",2,"3"])
+IO.puts SumMix.sumMin(["1","2","3"])
