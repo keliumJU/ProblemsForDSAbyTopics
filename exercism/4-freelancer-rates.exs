029bac5e532c79e7c9203a1737ea530f021cd277
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-01-30 19:59:21 -0500

    Rust begin

--- exercism/4-freelancer-rates.exs
+++ exercism/4-freelancer-rates.exs
@@ -1,6 +1,6 @@
 defmodule FreelancerRates do
   def daily_rate(hour_rate) do
-    hour_rate * 8
+    (hour_rate+(0.0)) * 8
   end
 
   def apply_discount(price, percent) do
@@ -10,6 +10,8 @@ defmodule FreelancerRates do
   def monthly_rate(hourly_rate, discount) do
     with_discount = apply_discount(hourly_rate, discount)
     daily_rate(with_discount) * 22
+    |> Float.ceil(2)
+    |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
@@ -20,4 +22,5 @@ defmodule FreelancerRates do
 end
 
 IO.inspect FreelancerRates.days_in_budget(20000, 80, 11.0)
-IO.inspect FreelancerRates.montly(20000, 80, 11.0)
+IO.inspect FreelancerRates.monthly_rate(65.2, 0.0)
+IO.inspect FreelancerRates.apply_discount(111.11, 13.5)
