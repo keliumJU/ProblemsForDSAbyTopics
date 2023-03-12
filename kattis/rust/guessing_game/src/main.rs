c0f8babdd3ef95c834cde9852d8b9f891f0c722d
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-11 23:59:01 -0500

    Add Ordering for guess number

--- kattis/rust/guessing_game/src/main.rs
+++ kattis/rust/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -16,6 +17,12 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number.to_string()) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
 }
