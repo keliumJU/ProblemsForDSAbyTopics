b182e81e4e5d54ac519be435dca84265f34582a5
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-19 23:56:21 -0500

    Add basic types in rust part 1

--- kattis/rust/variables/src/main.rs
+++ kattis/rust/variables/src/main.rs
@@ -18,4 +18,33 @@ fn main() {
 
     //Scalar types
     //integer types
+    // i32, u32, i8, u8, isize, usize ...
+    //Integer overflow
+    //-> wrapping_*, wrapping_add, overflowing_*, saturating_*, ...
+    //Floating-point types
+    //-> f32, f64 ==> all signed
+
+    let _zf = 2.0; //f64 (double precision float)
+    let _yf: f32 = 3.0; //f32
+
+    //Numeric operations
+    //adition
+    let _sum = 5 + 10;
+
+    //sutraction
+    let _difference = 95.5 - 4.3;
+
+    //miltiplication
+    let _product  = 4 * 30;
+
+    //division
+    let _qutient = 56.7 / 32.2;
+    let _truncated = -5 / 3;
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    //The boolean Type
+    let _t = true;
+    let _f: bool = false;
 }
