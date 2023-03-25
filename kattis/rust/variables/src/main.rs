97fbc7b92d6b4f05f4bfd5fc58bee00134da01b0
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-24 21:23:54 -0500

    Add more types: tuple, array, char

--- kattis/rust/variables/src/main.rs
+++ kattis/rust/variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
@@ -47,4 +49,51 @@ fn main() {
     //The boolean Type
     let _t = true;
     let _f: bool = false;
+
+    //The character type
+    let _c = 'z';
+    let _z: char = 'Z';
+    let _hearat_eyed_cat = '^';
+
+    //Compound types -> The Tuple Type
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    //pattern matching to destructure a tuple value :)
+    let (x, y, z) = tup;
+    println!("The value of y is: {y}");
+    println!("The value of x is: {x}");
+    println!("The value of z is: {z}");
+
+    let tx: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = tx.0;
+    let _six_point_four = tx.1;
+    let _one = tx.2;
+    
+    //The array type, fixed size
+    let _a = [1,2,3,4,5];
+    let _months = ["january", "february", "march"]; // ...
+    let _a: [i32; 5] = [1,2,3,4,5];
+    //same value for each element
+    let _a = [3; 5];
+    //accessing Array elements
+    let _first = _a[0];
+    let _second = _a[1];
+
+    let a = [1, 2, 3 ,4, 5];
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}")
+
 }
