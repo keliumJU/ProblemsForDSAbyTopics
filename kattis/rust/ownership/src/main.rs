674c9ac03e663cb648803067c74a71ac91b41860
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-04-09 02:19:04 -0500

    Add return values and ownership in functions

--- kattis/rust/ownership/src/main.rs
+++ kattis/rust/ownership/src/main.rs
@@ -41,4 +41,45 @@ fn main() {
     let y = x;
 
     println!("x = {}, y = {}", x, y);
+
+    //Ownership and functions
+    //Passing  a variable to a function will move or copy.
+    let ss = String::from("hello");
+    takes_ownership(ss);
+    
+    let xx = 5;
+    makes_copy(xx);
+
+    //Return values and scope
+    let _s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let _s3 = takes_and_gives_back(s2);
+
+    //Example ownership with tuple
+    let s11 = String::from("hello");
+    let (s2, len) = calculate_length(s11);
+    println!("The lenght of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
