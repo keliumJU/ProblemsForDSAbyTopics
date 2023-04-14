fd7a89f543ea0e2d010630a1ed463645dabc4f25
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-04-13 23:53:04 -0500

    Add mutable references topic

--- kattis/rust/references/src/main.rs
+++ kattis/rust/references/src/main.rs
@@ -4,6 +4,16 @@ fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
     println!("The length of '{}' is {}.", s1, len);
+
+
+    //Mutable references
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn calculate_length(s: &String) -> usize {
