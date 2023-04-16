db2ea9f25e681572f560b387e8e97f58b951b2e8
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-04-15 23:55:32 -0500

    Add topic references with scope and mut references or multiple rerefences

--- kattis/rust/references/src/main.rs
+++ kattis/rust/references/src/main.rs
@@ -12,15 +12,43 @@ fn main() {
     println!("{}", s);
 
     //Error code
+    /* 
     let mut s2 = String::from("hello");
     let r2 = &mut s2;
     let r3 = &mut s2;
 
     println!("{}, {}", r2, r3);
+    */
 
     //it's not posible double refrence, this restriction, preventing multiple
     //mutable references to the same data at the same time allows for mutation but in a very controlled fashion
-    
+
+    //references when the variable it's out of scope
+    let mut s2 = String::from("Hello");
+    {
+        let r1 = &mut s;
+    }// r1 goes out of scope here, so we can make a new references with no problems.
+    let r2 = &mut s;
+
+    //note: we cannot have a mutable reference while we have an immutable one to the same value
+    /* 
+        let r1 = &s1;
+        let r2 = &s1;
+        let r3 = &mut s2;
+    */
+    //only we can use separatly
+
+    let mut s = String::from("hello");
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    //Dangling References
+
 }
 
 fn change(some_string: &mut String) {
