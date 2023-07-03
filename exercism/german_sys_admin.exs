66572495ea2c49670996ff715e5b78f023b476e0
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-07-02 19:34:50 -0500

    Fix the german_sys_admin problem

--- exercism/german_sys_admin.exs
+++ exercism/german_sys_admin.exs
@@ -1,10 +1,22 @@
 defmodule Username do
+  @latin_substitute ['ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ß': 'ss']
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    username
+    |> Enum.map(fn c ->
+      if c in ~c"abcdefghijklmnopqrstuvwxyz_ßäöü" do
+        [Keyword.get(@latin_substitute, List.to_atom([c]), replace(c))]
+      end
+    end)
+    |> Enum.join()
+    |> String.to_charlist()
   end
+
+  def replace(char) when (char < 97 or char > 122) and char != 95, do: ''
+  def replace(char), do: char
 end
+
+
+IO.inspect(Username.sanitize(~c""))
+IO.inspect(Username.sanitize(~c"cäcilie_weiß"))
+#IO.inspect(Username.sanitize('mark_fischer$$$'))
+#IO.inspect(Username.sanitize('schmidt1985'))
