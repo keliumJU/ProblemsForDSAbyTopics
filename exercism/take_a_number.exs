152d7a1dbafea6ef37005530601bfd3fa6f28a7a
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-07-23 23:41:30 -0500

    Add final part for take_a_number problem

--- exercism/take_a_number.exs
+++ exercism/take_a_number.exs
@@ -1,12 +1,23 @@
 defmodule TakeANumber do
   parent = self()
   def start() do
-    spawn(fn -> send(parent, {0, self()}) end)
+    spawn(fn -> loop(0) end)
   end
 
-  def loop() do
+  def loop(last_number) do
     receive do
-      #....rest
+      {:report_state, sender} ->
+        send(sender, last_number)
+        loop(last_number)
+      {:take_a_number, sender} ->
+        send(sender, last_number+1)
+        loop(last_number+1)
+      :stop -> nil
+      _ -> loop(last_number)
     end
   end
 end
+
+machine_pid = TakeANumber.start()
+dbg(machine_pid)
+dbg(send(machine_pid, {:report_state, self()}))
